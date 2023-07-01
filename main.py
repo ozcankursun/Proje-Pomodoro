a014ffc59bb790d5829d8676de1b56586937de10
ozcankursun <ozcankursun@gmail.com>
2023-07-01 14:35:12 +0200

    test

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class LoginUI(QDialog):
         
 
 class MainMenuUI(QDialog):
-    #Bunu yapacagiz
+    #Bunu yapacagiz (Ozcan)
     def __init__(self):
         super(MainMenuUI,self).__init__()
         loadUi("./UI/mainMenu.ui",self)
