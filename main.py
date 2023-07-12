62fc4b9e49b6535686646f15170467ecf8473d34
Ademkrkt <krktadem@gmail.com>
2023-07-12 11:02:05 +0200

    zaman_sayaci_eklendi

--- main.py
+++ main.py
@@ -106,59 +106,175 @@ class MainMenuUI(QDialog):
             
 
 class PomodoroUI(QDialog):
-    # kodlar neredeyse hazir
-    #Yunus (Emre)
     def __init__(self):
         super(PomodoroUI,self).__init__()
         loadUi("./UI/pomodoro.ui",self)
-        #self.taskInput.clicked.connect(self.taskInputClicked)
-        # self.addTask.clicked.connect(self.addData) # !!!!!!!!!
-        # self.doneButton.clicked.connect(self.succes)
-        # self.labelAsNotFinishedButton.clicked.connect(self.fail)
-        # self.goToMainMenuButton.clicked.connect(self.go_main_menu)
-        self.startStopButton.clicked.connect(self.startTimer)
+        self.tur = 0
+        self.bitis = 4
+        self.addTask.clicked.connect(self.addData)
+        self.doneButton.clicked.connect(self.succes)
+        self.labelAsNotFinishedButton.clicked.connect(self.fail)
+        self.goToMainMenuButton.clicked.connect(self.main)
+
+
+        self.tur = 0
+        self.bitis = 4
+        while self.tur<4:
+            self.tur += 1  # Increment the current break cycle
+            print(self.tur)
+            
+            self.startStopButton.clicked.connect(self.startTimer)
+        
+        
+        #self.tasksCombo.clicked.connect(self.drop_down_menu) 
 
         self.timer = QTimer()
         self.timer.setInterval(1000)  # Her bir saniye için tetikleme
         self.timer.timeout.connect(self.updateTime)
-        self.remainingTime = 25 * 60  # Başlangıçta 25 dakika
+        self.remainingTime = 1 * 3 # Başlangıçta 25 dakika
 
-        #def taskInputClicked(self): #!!!!!!!
-        #task = self.taskInput.text()
-
-
-
-    #def addData(self):
-    #     conn = sqlite3.connect('./data.db')
-    #     curr = conn.cursor()
+        # self.tur= 0
+        # self.bitis= 4
     
-    #     # Kullanicinin girdigi task 'task' degiskenine atanir
-    #     task = self.taskInput.text()
+    def main(self):
+        main_menu = MainMenuUI()
+        widget.addWidget(main_menu)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
-    #     # database'e ekleme islemi
-    #     curr.execute("INSERT INTO tasks (task_name) VALUES (?)", (task,))
+    def addData(self):
+        conn= sqlite3.connect('data.db')
+        curr= conn.cursor()
+        task_name= self.taskInput.text()
+        # Veritabanında proje adını sorgulama
+        curr.execute("SELECT * FROM project WHERE project_name=?",(task_name,))
+        result = curr.fetchone()
+
+
+        if result is not None:
+            self.errorTextProjectLabel.setText('Bu proje veritabanında mevcut.')
+        else:
+            # Proje bilgilerini veritabanına ekliyor.
+            curr.execute("INSERT INTO project (project_name) VALUES (?)", (task_name,))
+            conn.commit()
+            self.errorTextProjectLabel.setText('Proje veritabanına eklendi.')
+        # Veritabanı bağlantısını kapat
+        conn.close()
+
+
+    def succes(self):
+        conn = sqlite3.connect("database.db")
+        curr = conn.cursor()
+        veri = True
+        curr.execute("INSERT INTO session (succes) VALUES (?)", (veri,))
+        conn.commit()
+        conn.close()
+
+    def fail(self):
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+        veri = False
+        cursor.execute("INSERT INTO session (succes) VALUES (?)", (veri,))
+        conn.commit()
+        conn.close()
+
     
-    #     # deigisklikler kaydedilip baglanti kapatilir
-    #     conn.commit()
-    #     conn.close()
+
+    def startTimer(self):
+        self.timer.start()
+        print(self.tur)
+       
+        
+    
+    def updateTime(self):
+        self.remainingTime -= 1
+        minutes = self.remainingTime // 60
+        seconds = self.remainingTime % 60
+        self.timeLabel.setText(f"{minutes:02d}:{seconds:02d}")
+        
+
+        if self.remainingTime == 0:
+            self.timer.stop()
+            print("Bölüm tamamlandı.")
+            
+
+            if self.tur == self.bitis:
+                self.go_to_long_break()
+            else:
+                self.go_to_short_break()
 
 
-    # def succes(self):
-    #     conn = sqlite3.connect("database.db")
-    #     cursor = conn.cursor()
-    #     veri = True
-    #     cursor.execute("INSERT INTO session (succes) VALUES (?)", (veri,))
-    #     conn.commit()
-    #     conn.close()
 
-    # def fail(self):
-    #     conn = sqlite3.connect("database.db")
-    #     cursor = conn.cursor()
-    #     veri = False
-    #     cursor.execute("INSERT INTO session (succes) VALUES (?)", (veri,))
-    #     conn.commit()
-    #     conn.close()
+    # def startTimer(self):
+    #     self.tur= 4
+    #     self.timer.start()
+    #     #self.tur -= 1
+        
+    # def updateTime(self):
+    #     self.remainingTime -= 1
+    #     minutes = self.remainingTime // 60
+    #     seconds = self.remainingTime % 60
+    #     self.timeLabel.setText(f"{minutes:02d}:{seconds:02d}")
+        
+        # if self.remainingTime == 0 and self.tur==self.bitis:
+        #     self.timer.stop()
+        #     print("Bolum tamamlandi.")
+        #     self.go_to_long_break() #go_to_long_break
+        # else:
+        #     self.go_to_short_break()
 
+        # if self.remainingTime == 0:
+        #     self.timer.stop()
+        #     print("Bölüm tamamlandı.")
+        #     self.tur -=1
+        #     if self.tur == 0:
+        #         self.go_to_long_break()
+        #     else:
+        #         self.go_to_short_break()
+        # else:
+        #     self.go_to_short_break()
+
+
+
+
+    def go_to_long_break(self):
+        long_break_page = LongBreakUI()
+        widget.addWidget(long_break_page)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def go_to_short_break(self):
+        short_break_page = ShortBreakUI()
+        widget.addWidget(short_break_page)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+        
+
+
+class ShortBreakUI(QDialog):
+    def __init__(self):
+        super(ShortBreakUI,self).__init__()
+        loadUi("./UI/shortBreak.ui",self)
+        self.setWindowTitle("Short Break")
+        self.goToMainMenuButton.clicked.connect(self.go_to_main_menu)
+        self.skipButton.clicked.connect(self.skip)
+        self.startButton.clicked.connect(self.startTimer)
+        
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)  # Her bir saniye için tetikleme
+        self.timer.timeout.connect(self.updateTime)
+        self.remainingTime = 1 * 3  # 5 dakika olmasi icin
+
+
+    def go_to_main_menu (self):
+        main_menu = MainMenuUI()
+        widget.addWidget(main_menu)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def skip (self):
+        pomodoro_page= PomodoroUI ()
+        widget.addWidget(pomodoro_page)
+        widget.setCurrentIndex(widget.currentIndex() +1)
+    
     def startTimer(self):
         self.timer.start()
         
@@ -171,24 +287,54 @@ class PomodoroUI(QDialog):
         if self.remainingTime == 0:
             self.timer.stop()
             print("Bolum tamamlandi.")
+            self.pomodoro()
 
+    def pomodoro(self):
+        pomodoro_page= PomodoroUI()
+        widget.addWidget(pomodoro_page)
+        widget.setCurrentIndex(widget.currentIndex()+1)
 
-class ShortBreakUI(QDialog):
-    def __init__(self):
-        super(ShortBreakUI,self).__init__()
-        loadUi("./UI/shortBreak.ui",self)
-        self.goToMainMenuButton.clicked.connect(self.go_main_menu)
-        self.startButton.clicked.connect(self.startTimer)
-
-        self.timer = QTimer()
-        self.timer.setInterval(1000)  # Her bir saniye için tetikleme
-        self.timer.timeout.connect(self.updateTime)
-        self.remainingTime = 5 * 60  # 5 dakika olmasi icin
+            
 
 class LongBreakUI(QDialog):
     def __init__(self):
         super(LongBreakUI,self).__init__()
         loadUi("./UI/longBreak.ui",self)
+        self.goToMainMenuButton.clicked.connect(self.go_to_main_menu)
+        self.skipButton.clicked.connect(self.pomodoro_page)
+        self.startButton.clicked.connect(self.startTimer)
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)  # Her bir saniye için tetikleme
+        self.timer.timeout.connect(self.updateTime)
+        self.remainingTime = 1 * 3  # 5 dakika olmasi icin
+
+    def go_to_main_menu(self):
+        main= MainMenuUI()
+        widget.addWidget(main)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def pomodoro_page(self):
+        pomodoro= PomodoroUI()
+        widget.addWidget(pomodoro)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+    
+    def startTimer(self):
+        self.timer.start()
+        
+    def updateTime(self):
+        self.remainingTime -= 1
+        minutes = self.remainingTime // 60
+        seconds = self.remainingTime % 60
+        self.timeLabel.setText(f"{minutes:02d}:{seconds:02d}")
+        
+        if self.remainingTime == 0:
+            self.timer.stop()
+            print("Bolum tamamlandi.")
+            self.pomodoro_page()
+
+    
+
 
 
 app = QApplication(sys.argv)
